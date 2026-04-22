ab32ec7bcb174bdbd231c83555b21d053e55651c
Alex Frutos <afrutos@hpc-ext-3.cam.uchc.edu>
2026-04-22 11:23:33 -0400

    Align short and long reads to Pilon assembly

---
+++ scripts/07_pilon_alignment.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=pilon_align
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --mem=32G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-user=alex.frutos@uconn.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+module load bwa/0.7.17
+module load minimap2
+module load samtools
+
+REF=../data/pilon_polish/pilon_polished.fasta
+trim_dir=../data/trimmed
+ONT_reads=../data/raw/ONT_raw/*fastq.gz
+out_dir=../data/pilon_polish/ILL_alignment
+ONT_out_dir=../data/pilon_polish/ONT_alignment
+mkdir -p $out_dir
+mkdir -p $ONT_out_dir
+
+bwa index $REF
+
+for R1 in $trim_dir/*_1_trimmed.fastq.gz; do
+    base=$(basename $R1 _1_trimmed.fastq.gz)
+    R2=$trim_dir/${base}_2_trimmed.fastq.gz
+
+    bwa mem -t 8 $REF $R1 $R2 | samtools sort -@ 8 -o $out_dir/${base}.sorted.bam
+    samtools index $out_dir/${base}.sorted.bam
+done
+
+for FQ in $ONT_reads; do
+    base=$(basename $FQ .fastq.gz)
+
+    minimap2 -ax map-ont -t 8 $REF $FQ | \
+    samtools view -bS - | \
+    samtools sort -@ 8 -o $ONT_out_dir/${base}.sorted.bam
+
+    samtools index $ONT_out_dir/${base}.sorted.bam
+done
